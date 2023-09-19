6d4dc4af0497c0acf1a64ff8abcc5a352c557a1a
Lucas Vizeu <nota10.lucas@gmail.com>
2023-09-19 16:19:05 -0300

    3.3
    exe 1

---
+++ Exercicios-Aula-3/src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bem Vindo ao meu banco sem nenhuma semelhança com a realidade");
+
+        Conta conta = new Conta(0.0, );
+        Conta conta2 = new Conta();
+        //Interage com os métodos
+        conta.deposito(200.0);
+        System.out.println(armario.retornaSaldo());
+
+
+    }
+    }
